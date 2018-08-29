14cb9cc954d2c4c8a07975cb60c653fbdec1e724
t1mmmmY <t1mmmmY111@gmail.com>
2018-08-29 18:44:06 +0200

    TapJoy integration

---
+++ Assets/Game/Scripts/Network/AdvertisingAgent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TapjoyUnity;
+
+public class AdvertisingAgent : MonoBehaviour 
+{
+	void Start () 
+	{
+		if (!Tapjoy.IsConnected) 
+		{
+			Tapjoy.Connect();
+		}
+	}
+
+	void ShowVideo()
+	{
+	}
+}
